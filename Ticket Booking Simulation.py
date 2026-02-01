d3170e4518e7a2c9b2b1408b98583a8c026c290b
Shivaraj H <140573544+shivuhombanna@users.noreply.github.com>
2026-02-01 20:14:24 +0530

    Uncomment ticket booking and countdown logic

--- Ticket Booking Simulation.py
+++ Ticket Booking Simulation.py
@@ -1,17 +1,17 @@
-# seats = 8
+seats = 8
 
-# while seats > 0:
-#     print(f" {seats}avelabal sites ")
-#     booking=input("booking (yes/no)").lower()
+while seats > 0:
+    print(f" {seats}avelabal sites ")
+    booking=input("booking (yes/no)").lower()
 
-#     if booking == "yes":
-#         seats -=1
-#         print(f"awailabal")
-#     elif booking=="no":
-#         print("not booking cansal")
-#         break
-#     else:
-#         print("all setes or booked")
+    if booking == "yes":
+        seats -=1
+        print(f"awailabal")
+    elif booking=="no":
+        print("not booking cansal")
+        break
+    else:
+        print("all setes or booked")
 
 #new yer count down
 i=10
@@ -22,3 +22,4 @@ while i>=1:
         x +=1
     print("")
     i -=1
+
