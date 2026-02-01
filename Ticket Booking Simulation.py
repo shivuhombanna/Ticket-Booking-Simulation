c864b4b57a74a17570f6aebd9ee62c9042f1c532
Shivaraj H <140573544+shivuhombanna@users.noreply.github.com>
2026-02-01 20:13:23 +0530

    Add files via upload

---
+++ Ticket Booking Simulation.py
@@ -0,0 +1,24 @@
+# seats = 8
+
+# while seats > 0:
+#     print(f" {seats}avelabal sites ")
+#     booking=input("booking (yes/no)").lower()
+
+#     if booking == "yes":
+#         seats -=1
+#         print(f"awailabal")
+#     elif booking=="no":
+#         print("not booking cansal")
+#         break
+#     else:
+#         print("all setes or booked")
+
+#new yer count down
+i=10
+while i>=1:
+    x=1
+    while x<=i:
+        print(x,"happy new year " , end="-")
+        x +=1
+    print("")
+    i -=1
